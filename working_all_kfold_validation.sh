eab651f64194f0694772b3a1c3bc1f76d0fd67b8
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-03-07 18:14:12 -0300

    ordenando

--- working_all_kfold_validation.sh
+++ working_all_kfold_validation.sh
@@ -1,4 +1,5 @@
-REM Export `kfold_validation.ipynb` to `kfold_validation.py`
+
+# Export `kfold_validation.ipynb` to `kfold_validation.py`
 jupyter nbconvert --to python kfold_validation.ipynb
 
 python kfold_validation.py --model efficientnet_b3
@@ -7,4 +8,4 @@ python kfold_validation.py --model inception_v3
 python kfold_validation.py --model inception_resnet_v2
 python kfold_validation.py --model resnet_v2_50
 
-del /Q kfold_validation.py
+rm -f kfold_validation.py
