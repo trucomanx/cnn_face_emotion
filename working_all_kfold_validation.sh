39ebb97147b8ffd7ddf37ad43f5a230f98f4f9be
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-03-08 17:36:38 -0300

    ordenando

--- working_all_kfold_validation.sh
+++ working_all_kfold_validation.sh
@@ -7,5 +7,9 @@ python kfold_validation.py --model efficientnet_b3
 python kfold_validation.py --model inception_v3
 python kfold_validation.py --model inception_resnet_v2
 python kfold_validation.py --model resnet_v2_50
+python kfold_validation.py --model custom1
+python kfold_validation.py --model custom_dense1
+python kfold_validation.py --model custom_residual1
+python kfold_validation.py --model custom_inception
 
 rm -f kfold_validation.py
