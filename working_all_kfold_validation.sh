491416d7059e658124b2f0e04866787cd5eea7a2
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-03-13 13:43:05 -0300

    ordenando

--- working_all_kfold_validation.sh
+++ working_all_kfold_validation.sh
@@ -3,13 +3,13 @@
 jupyter nbconvert --to python kfold_validation.ipynb
 
 #python kfold_validation.py --model mobilenet_v3
-python kfold_validation.py --model efficientnet_b3
+#python kfold_validation.py --model efficientnet_b3
 python kfold_validation.py --model inception_v3
-python kfold_validation.py --model inception_resnet_v2
-python kfold_validation.py --model resnet_v2_50
-python kfold_validation.py --model custom1
-python kfold_validation.py --model custom_dense1
-python kfold_validation.py --model custom_residual1
-python kfold_validation.py --model custom_inception
+#python kfold_validation.py --model inception_resnet_v2
+#python kfold_validation.py --model resnet_v2_50
+#python kfold_validation.py --model custom1
+#python kfold_validation.py --model custom_dense1
+#python kfold_validation.py --model custom_residual1
+#python kfold_validation.py --model custom_inception
 
 rm -f kfold_validation.py
