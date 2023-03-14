2f22349b0d52f0301fd56248ae9e4e27ee42e203
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-03-14 09:16:34 -0300

    ordenando

--- working_all_kfold_validation.sh
+++ working_all_kfold_validation.sh
@@ -2,7 +2,6 @@
 # Export `kfold_validation.ipynb` to `kfold_validation.py`
 jupyter nbconvert --to python kfold_validation.ipynb
 
-
 #python kfold_validation.py --model mobilenet_v3        --epochs 50  --batch-size 32
 #python kfold_validation.py --model efficientnet_b3     --epochs 50  --batch-size 4
 python kfold_validation.py --model inception_v3        --epochs 100 --batch-size 4
