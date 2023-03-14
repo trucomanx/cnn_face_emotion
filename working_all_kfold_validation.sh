41dd1b71ec89da82155c90bae5c86fbbba95c139
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-03-14 02:51:53 -0300

    agregando compatiblidad inter OS

--- working_all_kfold_validation.sh
+++ working_all_kfold_validation.sh
@@ -3,14 +3,14 @@
 jupyter nbconvert --to python kfold_validation.ipynb
 
 
-#python kfold_validation.py --model mobilenet_v3
-#python kfold_validation.py --model efficientnet_b3
-python kfold_validation.py --model inception_v3
-#python kfold_validation.py --model inception_resnet_v2
-#python kfold_validation.py --model resnet_v2_50
-#python kfold_validation.py --model custom1
-#python kfold_validation.py --model custom_dense1
-#python kfold_validation.py --model custom_residual1
-#python kfold_validation.py --model custom_inception
+#python kfold_validation.py --model mobilenet_v3        --epochs 50  --batch-size 32
+#python kfold_validation.py --model efficientnet_b3     --epochs 50  --batch-size 4
+python kfold_validation.py --model inception_v3        --epochs 100 --batch-size 4
+#python kfold_validation.py --model inception_resnet_v2 --epochs 50  --batch-size 32
+#python kfold_validation.py --model resnet_v2_50        --epochs 50  --batch-size 32
+#python kfold_validation.py --model custom1             --epochs 50  --batch-size 32
+#python kfold_validation.py --model custom_dense1       --epochs 50  --batch-size 32
+#python kfold_validation.py --model custom_residual1    --epochs 50  --batch-size 32
+#python kfold_validation.py --model custom_inception    --epochs 50  --batch-size 32
 
 rm -f kfold_validation.py
