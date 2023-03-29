60c17741710cbeb97753083096da559d0fa6ab85
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-03-29 13:57:16 -0300

    agregando caompatiblidad varios dataset

--- working_all_kfold_validation.sh
+++ working_all_kfold_validation.sh
@@ -1,14 +1,16 @@
 # Export `kfold_validation.ipynb` to `kfold_validation.py`
 jupyter nbconvert --to python kfold_validation.ipynb
 
-#python3 kfold_validation.py --model mobilenet_v3        --epochs 50  --batch-size 32
-#python3 kfold_validation.py --model efficientnet_b3     --epochs 30  --batch-size 64
-python3 kfold_validation.py --model inception_v3        --epochs 50 --batch-size 32
-#python3 kfold_validation.py --model inception_resnet_v2 --epochs 50  --batch-size 32
-#python3 kfold_validation.py --model resnet_v2_50        --epochs 50  --batch-size 32
-#python3 kfold_validation.py --model custom1             --epochs 50  --batch-size 32
-#python3 kfold_validation.py --model custom_dense1       --epochs 50  --batch-size 32
-#python3 kfold_validation.py --model custom_residual1    --epochs 50  --batch-size 32
-#python3 kfold_validation.py --model custom_inception    --epochs 50  --batch-size 32
+DATASET='fer2013'
+
+#python3 kfold_validation.py --model mobilenet_v3        --dataset $DATASET --epochs 50  --batch-size 32
+ python3 kfold_validation.py --model efficientnet_b3     --dataset $DATASET --epochs 50  --batch-size 16 #--model efficientnet_b3     --epochs 30  --batch-size 64
+#python3 kfold_validation.py --model inception_v3        --dataset $DATASET --epochs 50 --batch-size 32
+#python3 kfold_validation.py --model inception_resnet_v2 --dataset $DATASET --epochs 50  --batch-size 32
+#python3 kfold_validation.py --model resnet_v2_50        --dataset $DATASET --epochs 50  --batch-size 32
+#python3 kfold_validation.py --model custom1             --dataset $DATASET --epochs 50  --batch-size 128 #--epochs 75  --batch-size 32
+#python3 kfold_validation.py --model custom_dense1       --dataset $DATASET --epochs 50  --batch-size 32
+#python3 kfold_validation.py --model custom_residual1    --dataset $DATASET --epochs 50  --batch-size 32
+#python3 kfold_validation.py --model custom_inception    --dataset $DATASET --epochs 50  --batch-size 32
 
 rm -f kfold_validation.py
