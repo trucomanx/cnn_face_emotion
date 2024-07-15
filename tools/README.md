dfaa78d7e896b3d6aa81dc7ce00349c261a8c72f
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-07-15 16:13:15 -0300

    ordenando

---
+++ tools/README.md
@@ -0,0 +1,5 @@
+
+
+* csv_dataset_image_verificator.py 
+
+Lee um aquivo train.csv e verifica se cada file listado existe e verifica se o formato e' PNG.
--- tools/csv_dataset_image_verificator.py
+++ tools/csv_dataset_image_verificator.py
@@ -6,7 +6,40 @@ from PIL import Image
 
 # Ruta al archivo CSV
 csv_file_path = 'train.csv'
+image_format = 'PNG'
 
+import argparse
+
+parser = argparse.ArgumentParser(   prog='ProgramName',
+                                    description='Lee um aquivo train.csv e verifica se cada file listado existe e verifica se o formato e` PNG.',
+                                    epilog='Text at the bottom of help',
+                                    add_help=False)
+
+# Definindo os argumentos
+parser.add_argument('--input'  , type=str, default=csv_file_path, help='Caminho do arquivo *.cvs de entrada')
+parser.add_argument('--format' , type=str, default=image_format , help='Formato dos arquivos de imagem de entrada, retorna True se o formato e assim.')
+parser.add_argument('--verbose', action='store_true', help='Exibir informações detalhadas.')
+parser.add_argument('--help', action='store_true',  help='Show this help message and exit.')
+
+# Parse dos argumentos
+args = parser.parse_args()
+
+# Usando os argumentos
+if args.verbose:
+    print(f"Arquivo de entrada: {args.input}")
+    print(f"Formato de imagem: {args.format}")
+    print('')
+    
+if args.help:
+    parser.print_help();
+    exit();
+################################################################################
+
+if os.path.exists(args.input)==False:
+    print('No exist',args.input)
+    exit();
+    
+    
 # Leer el archivo CSV usando pandas
 df = pd.read_csv(csv_file_path)
 
@@ -17,7 +50,7 @@ def is_valid_png(file_path):
     if os.path.exists(file_path):
         try:
             with Image.open(file_path) as img:
-                return img.format == 'PNG'
+                return img.format == args.format
         except IOError:
             return False
     return False
