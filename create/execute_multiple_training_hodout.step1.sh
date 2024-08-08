805a84a498d1c540150baae9e7a8f06f8ed2f599
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-08-08 13:14:06 -0300

    ordenando

--- create/execute_multiple_training_hodout.step1.sh
+++ create/execute_multiple_training_hodout.step1.sh
@@ -28,12 +28,12 @@ OutDir='/mnt/8811f502-ae19-4dd8-8371-f1915178f581/Fernando/OUTPUTS/DOCTORADO2/cn
 
   
 DName='multiple_ber2024-face'  
-
+SubDir='multiple_training_validation_holdout_step1'
 
 ################################################################################
 
-mkdir -p $OutDir/$DName/'multiple_training_validation_holdout'
-echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/'multiple_training_validation_holdout/main.py'
+mkdir -p $OutDir/$DName/$SubDir
+echo "$PyCommand" | cat - 'main.py' > temp && mv temp $OutDir/$DName/$SubDir/'main.py'
 
 ################################################################################
 export TF_USE_LEGACY_KERAS=1 
@@ -41,11 +41,11 @@ export CUDA_VISIBLE_DEVICES=0
 
 ipynb-py-convert multiple_training_hodout.ipynb multiple_training_hodout.py
 
-python3 multiple_training_hodout.py --model 'efficientnet_b3'     --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir
-python3 multiple_training_hodout.py --model 'inception_resnet_v2' --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir
-python3 multiple_training_hodout.py --model 'inception_v3'        --epochs 100 --patience 25 --batch-size  16 --seed 12 --dataset-name $DName --output-dir $OutDir
-python3 multiple_training_hodout.py --model 'mobilenet_v3'        --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir
-python3 multiple_training_hodout.py --model 'resnet_v2_50'        --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir
+python3 multiple_training_hodout.py --model 'efficientnet_b3'     --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir --subdir $SubDir
+python3 multiple_training_hodout.py --model 'inception_resnet_v2' --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir --subdir $SubDir
+python3 multiple_training_hodout.py --model 'inception_v3'        --epochs 100 --patience 25 --batch-size  16 --seed 12 --dataset-name $DName --output-dir $OutDir --subdir $SubDir
+python3 multiple_training_hodout.py --model 'mobilenet_v3'        --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir --subdir $SubDir
+python3 multiple_training_hodout.py --model 'resnet_v2_50'        --epochs 100 --patience 20 --batch-size  16 --seed 42 --dataset-name $DName --output-dir $OutDir --subdir $SubDir
 
 rm -f multiple_training_hodout.py
 
