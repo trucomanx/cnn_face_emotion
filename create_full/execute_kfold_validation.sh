ae339d1629c96909d5d268fc8e825a88746511b3
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-12-14 19:50:20 -0300

    enviar

--- create_full/execute_kfold_validation.sh
+++ create_full/execute_kfold_validation.sh
@@ -57,11 +57,11 @@ export CUDA_VISIBLE_DEVICES=0
 
 ipynb-py-convert kfold_validation.ipynb kfold_validation.py
 
-python3 kfold_validation.py --model 'mobilenet_v3'        --epochs 100  --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'efficientnet_b3'     --epochs 100  --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'inception_v3'        --epochs 100  --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'inception_resnet_v2' --epochs 100  --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
-python3 kfold_validation.py --model 'resnet_v2_50'        --epochs 100  --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'mobilenet_v3'        --epochs 200 --patience 40 --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'efficientnet_b3'     --epochs 200 --patience 40 --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'inception_v3'        --epochs 200 --patience 40 --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'inception_resnet_v2' --epochs 200 --patience 40 --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
+python3 kfold_validation.py --model 'resnet_v2_50'        --epochs 200 --patience 40 --batch-size 16 --dataset-dir $InTrD --dataset-file $InTrF --dataset-name $DName --output-dir $OutDir
 
 rm -f kfold_validation.py
 
